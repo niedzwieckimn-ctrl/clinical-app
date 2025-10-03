d37f5ee87941dbbee6a23ae51125fa9ec641a2e9
niedzwieckimn-ctrl <niedzwiecki.mn@gmail.com>
2025-10-03 22:26:46 +0200

    Add files via upload

--- assets/supabase-client.js
+++ assets/supabase-client.js
@@ -1,5 +1,5 @@
 // === UZUPEŁNIJ TE DANE (z Supabase → Settings → API) ===
-const SB_URL  = 'https://eibzjpelnmvbtslquun.supabase.co';
+const SB_URL  = 'https://eibzijpelnmvbtslquun.supabase.co';
 const SB_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpYnppanBlbG5tdmJ0c2xxdXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTE1OTcsImV4cCI6MjA3NDE4NzU5N30.Dp4u9PlhP-_pGmiNTHp5zSjrMUDfA_k2i85_71_9koo';
 // ========================================================
 
